83290867c04e1334790315764fe5c1c98f92c936
PAWAN TIWARI <pawantiwari4091@gmail.com>
2026-08-24 01:13:38 +0530

    Time: 4 ms (47.51%) | Memory: 14.1 MB (9.98%) - LeetSync

--- 2039-sum-game/sum-game.cpp
+++ 2039-sum-game/sum-game.cpp
@@ -2,36 +2,36 @@ class Solution {
 public:
     bool sumGame(string num) {
         int n = num.length();
-        int leftKnownSum = 0;
-        int rightKnownSum = 0;
+        int leftSum = 0;
+        int rightSum = 0;
 
-        int leftQnMarkCount = 0;
-        int rightQnmarkCount = 0;
+        int leftCount = 0;
+        int rightCount = 0;
 
         for(int i = 0; i < n; i++) {
             if(num[i] == '?') {
                 if(i < n/2) {
-                    leftQnMarkCount++;
+                    leftCount++;
                 } else {
-                    rightQnmarkCount++;
+                    rightCount++;
                 }
             } else {
                 if(i < n/2) {
-                    leftKnownSum += num[i] - '0';
+                    leftSum += num[i] - '0';
                 } else {
-                    rightKnownSum += num[i] - '0';
+                    rightSum += num[i] - '0';
                 }
             }
         }
 
-        int totalQnMarks = leftQnMarkCount + rightQnmarkCount;
+        int totalQnMarks = leftCount + rightCount;
         if(totalQnMarks % 2 == 1) { //Odd - alice always wins
             return true;
         }
 
-        int LEFT = 2 * leftKnownSum + 9*leftQnMarkCount;
+        int LEFT = 2 * leftSum + 9*leftCount;
 
-        int RIGHT = 2 * rightKnownSum + 9*rightQnmarkCount;
+        int RIGHT = 2 * rightSum + 9*rightCount;
 
         if(LEFT == RIGHT) //Bob wins
             return false;
