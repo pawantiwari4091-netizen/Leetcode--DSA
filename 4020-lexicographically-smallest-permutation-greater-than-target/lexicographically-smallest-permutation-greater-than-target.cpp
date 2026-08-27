3a1c56ad55e89ecfca71065d7ed896c34eba5d91
PAWAN TIWARI <pawantiwari4091@gmail.com>
2026-08-27 22:27:41 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (94.90%) - LeetSync

--- 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
+++ 4020-lexicographically-smallest-permutation-greater-than-target/lexicographically-smallest-permutation-greater-than-target.cpp
@@ -3,20 +3,20 @@ public:
     string lexGreaterPermutation(string s, string target) {
         int n = s.length();
 
-        
+
         vector<int> cnt(26, 0);
         for (char c : s) {
             cnt[c - 'a']++;
         }
 
-       
+
         string res = string(n, ' ');
 
-        
+
         int i = 0;
         while (i < n) {
             char t_char = target[i];
-            
+
             if (cnt[t_char - 'a'] > 0) {
                 res[i] = t_char;
                 cnt[t_char - 'a']--;
@@ -26,28 +26,27 @@ public:
             }
         }
 
-    
         int limit = (i == n) ? n - 1 : i;
         for (int curr = limit; curr >= 0; curr--) {
             if (res[curr] != ' ') {
                 cnt[res[curr] - 'a']++;
                 res[curr] = ' '; 
             }
+
             char target_char = target[curr];
             int choice = -1;
             for (int c = (target_char - 'a') + 1; c < 26; c++) {
                 if (cnt[c] > 0) {
                     choice = c;
-                    break;
+                    break; 
                 }
             }
 
             if (choice != -1) {
-               
+                
                 res[curr] = (char)('a' + choice);
                 cnt[choice]--;
 
-             
                 int write_idx = curr + 1;
                 for (int c = 0; c < 26; c++) {
                     while (cnt[c] > 0) {
@@ -58,7 +57,7 @@ public:
                 }
                 return res; 
             }
- 
+
         }
         return "";
     }
