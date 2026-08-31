dd95ee68ec226ccb7f4388d581dee46e5bcfe6bb
PAWAN TIWARI <pawantiwari4091@gmail.com>
2026-08-31 16:48:40 +0530

    Time: 5 ms (39.86%) | Memory: 128.5 MB (17.95%) - LeetSync

---
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> nodesBetweenCriticalPoints(ListNode* head) {
+        vector<int> idx;
+
+        ListNode* prev = NULL;
+        int currIdx = 0;
+        while(head != NULL){
+            if(prev == NULL || head->next == NULL){
+                prev = head;
+                head = head->next;
+                continue;
+            }
+
+            if(head->val > prev->val && head->val > head->next->val) idx.push_back(currIdx);
+            else if(head->val < prev->val && head->val < head->next->val) idx.push_back(currIdx);
+
+            prev = head;
+            head = head->next;
+            currIdx++;
+        }
+
+        if(idx.size() == 0 || idx.size() == 1) return {-1, -1};
+
+        int mini = INT_MAX, maxi = INT_MIN;
+        maxi = idx[idx.size() - 1] - idx[0];
+        for(int i = 1; i < idx.size(); i++){
+            mini = min(mini, idx[i] - idx[i - 1]);
+        }
+        return {mini, maxi};
+    }
+};
