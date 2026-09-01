f20bcb230e905648f6bdd6fa3d88606bc5ed528b
PAWAN TIWARI <pawantiwari4091@gmail.com>
2026-09-01 20:56:08 +0530

    Time: 1148 ms (48.98%) | Memory: 414.9 MB (29.59%) - LeetSync

---
+++ 3870-minimum-moves-to-clean-the-classroom/minimum-moves-to-clean-the-classroom.cpp
@@ -0,0 +1,97 @@
+class Solution {
+public:
+    using VB = vector<bool>;
+    using VVB = vector<VB>;
+    using VVVB = vector<VVB>;
+    using VVVVB = vector<VVVB>;
+
+    int minMoves(vector<string>& classi, int energy) {
+        int n = classi.size();
+        int m = classi[0].size();
+
+        vector<vector<int>> idx(n, vector<int>(m));
+
+        int sr = 0, sc = 0;
+        int k = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+                if (classi[i][j] == 'S') {
+                    sr = i;
+                    sc = j;
+                } else if (classi[i][j] == 'L') {
+                    idx[i][j] = k++;
+                }
+            }
+        }
+
+        if (k == 0)
+            return 0;
+
+        int totalMaxNumber = (1 << k) - 1;
+
+        struct state {
+            int r, c, energy, moves, num;
+        };
+
+        queue<state> q;
+
+        VVVVB seen(n, VVVB(m, VVB(energy + 1, VB(1 << k, false))));
+
+        q.push({sr, sc, energy, 0, 0});
+        seen[sr][sc][energy][0] = true;
+
+        int dr[] = {-1, 0, 1, 0};
+        int dc[] = {0, 1, 0, -1};
+
+        while (!q.empty()) {
+
+            state curr = q.front();
+            q.pop();
+
+            for (int d = 0; d < 4; d++) {
+
+                int nr = curr.r + dr[d];
+                int nc = curr.c + dc[d];
+
+                if (nr < 0 || nc < 0 || nr >= n || nc >= m)
+                    continue;
+
+                if (classi[nr][nc] == 'X')
+                    continue;
+
+                int ne = curr.energy - 1;
+
+                if (ne < 0)
+                    continue;
+
+                int nnum = curr.num;
+
+                // Recharge
+                if (classi[nr][nc] == 'R') {
+                    ne = energy;
+                }
+
+                // Collect letter
+                if (classi[nr][nc] == 'L') {
+                    nnum |= (1 << idx[nr][nc]);
+                }
+
+                // All letters collected
+                if (nnum == totalMaxNumber) {
+                    return curr.moves + 1;
+                }
+
+                if (!seen[nr][nc][ne][nnum]) {
+
+                    seen[nr][nc][ne][nnum] = true;
+
+                    q.push({nr, nc, ne, curr.moves + 1, nnum});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
