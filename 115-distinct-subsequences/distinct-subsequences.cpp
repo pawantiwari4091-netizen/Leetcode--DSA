268ad69756b5cda9c670fc95d36c99e7a154b0d4
PAWAN TIWARI <pawantiwari4091@gmail.com>
2026-09-06 17:15:51 +0530

    Time: 29 ms (61.20%) | Memory: 44.4 MB (11.48%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int n, m;
-    vector<vector<long long>> dp;
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
 
-    long long solve(string &s, string &t, int i, int j) {
-        if(j == m)
-            return 1;
+        vector<vector<unsigned long long>> dp(n + 1,vector<unsigned long long>(m + 1, 0));
 
-        if(i == n)
-            return 0;
-
-        if(dp[i][j] != -1)
-            return dp[i][j];
-
-        long long ans = solve(s, t, i + 1, j); // skip
-
-        if(s[i] == t[j]) {
-            ans += solve(s, t, i + 1, j + 1); // take
+        // Empty t can always be formed in 1 way
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
         }
 
-        return dp[i][j] = ans;
-    }
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
 
-    int numDistinct(string s, string t) {
-        n = s.size();
-        m = t.size();
+                // Skip s[i-1]
+                dp[i][j] = dp[i-1][j];
 
-        dp.assign(n, vector<long long>(m, -1));
+                // Take s[i-1]
+                if(s[i-1] == t[j-1]) {
+                    dp[i][j] += dp[i-1][j-1];
+                }
+            }
+        }
 
-        return solve(s, t, 0, 0);
+        return dp[n][m];
     }
 };
