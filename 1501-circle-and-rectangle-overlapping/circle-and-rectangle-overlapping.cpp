6d1d6469735e954801fe5d611f77e44f52b129f4
PAWAN TIWARI <pawantiwari4091@gmail.com>
2026-09-20 03:09:01 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (95.11%) - LeetSync

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1, int x2, int y2) {
+/*
+        CLAMP method => Parameters
+        num      -    the value to clamp
+        low, high  -    the boundaries to clamp num to
+
+
+        Return value
+        Reference to low if num is less than low,
+        Reference to high if high is less than num,
+        otherwise reference to num.
+
+*/
         
         int xi = clamp(xCenter, x1, x2);
         int yi = clamp(yCenter, y1, y2);
